0decc9b510397851ab2f89469e62d8e785aa506c
Erick Garcia <erickarmandogarcia@hotrmail.com>
2017-02-16 00:39:29 -0700

    Demo working

--- competidoresDb.php
+++ competidoresDb.php
@@ -1,7 +1,7 @@
 <?php
   $competidoresList=array(
-    array("name"=>"nameComp1","lastName"=>"lastNameComp1"),
-    array("name"=>"nameComp2","lastName"=>"lastNameComp2")
+    array("name"=>"nameComp1","lastName"=>"lastNameComp1","score"=>0),
+    array("name"=>"nameComp2","lastName"=>"lastNameComp2","score"=>0)
   );
   echo json_encode($competidoresList)
 ?>
