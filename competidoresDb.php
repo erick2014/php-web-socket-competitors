6e58afc767a14a8b45141dda49c5ca55ff67d586
Erick Garcia <erickarmandogarcia@hotrmail.com>
2017-02-16 15:29:37 -0700

    styling for competidores.html, update competidoresDb.php, add some assets

--- competidoresDb.php
+++ competidoresDb.php
@@ -1,7 +1,15 @@
 <?php
   $competidoresList=array(
-    array("name"=>"nameComp1","lastName"=>"lastNameComp1","score"=>0),
-    array("name"=>"nameComp2","lastName"=>"lastNameComp2","score"=>0)
+    array("name"=>"Ronald Andrés","lastName"=>"Púlido Gutierrez","score"=>0),
+    array("name"=>"Reymer","lastName"=>"Ramírez","score"=>0),
+    array("name"=>"Andrés Felipe","lastName"=>"Fandiño","score"=>0),
+    array("name"=>"Camilo","lastName"=>"Roa","score"=>0),
+    array("name"=>"José Alfredo","lastName"=>"Rueda Benítez","score"=>0),
+    array("name"=>"Camilo","lastName"=>"Orozco","score"=>0),
+    array("name"=>"Óscar","lastName"=>"Arana Correa","score"=>0),
+    array("name"=>"Juan Esteban","lastName"=>"Quíntero","score"=>0),
+    array("name"=>"Juan Felipe","lastName"=>"Ortega Mejía","score"=>0),
+    array("name"=>"Juan David","lastName"=>"Arroyave","score"=>0)
   );
   echo json_encode($competidoresList)
 ?>
