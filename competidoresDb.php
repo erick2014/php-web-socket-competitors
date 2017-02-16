f48d38a66a8465b8544998365c89cf7a39e82b43
Erick Garcia <erickarmandogarcia@hotrmail.com>
2017-02-15 23:54:38 -0700

    Initial commit

---
+++ competidoresDb.php
@@ -0,0 +1,7 @@
+<?php
+  $competidoresList=array(
+    array("name"=>"nameComp1","lastName"=>"lastNameComp1"),
+    array("name"=>"nameComp2","lastName"=>"lastNameComp2")
+  );
+  echo json_encode($competidoresList)
+?>
