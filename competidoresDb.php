7e2699873d82a9d2583cbde634a23fa51388f6b8
Erick Garcia <erickarmandogarcia@hotrmail.com>
2017-02-17 09:50:40 -0700

    Add db functionality

--- competidoresDb.php
+++ competidoresDb.php
@@ -1,19 +1,41 @@
 <?php
   //get competitors from file
   if( $_SERVER['REQUEST_METHOD']=="GET" ){
-    $competidoresList=array(
-      array("name"=>"Ronald Andrés","lastName"=>"Púlido Gutierrez","score"=>0),
-      array("name"=>"Reymer","lastName"=>"Ramírez","score"=>0),
-      array("name"=>"Andrés Felipe","lastName"=>"Fandiño","score"=>0),
-      array("name"=>"Camilo","lastName"=>"Roa","score"=>0),
-      array("name"=>"José Alfredo","lastName"=>"Rueda Benítez","score"=>0),
-      array("name"=>"Camilo","lastName"=>"Orozco","score"=>0),
-      array("name"=>"Óscar","lastName"=>"Arana Correa","score"=>0),
-      array("name"=>"Juan Esteban","lastName"=>"Quíntero","score"=>0),
-      array("name"=>"Juan Felipe","lastName"=>"Ortega Mejía","score"=>0),
-      array("name"=>"Juan David","lastName"=>"Arroyave","score"=>0)
-    );
-    echo json_encode($competidoresList);
+    //db connection
+    $conn = mysqli_connect("127.0.0.1", "root", "123456", "xpartano");
+
+    if (!$conn) {
+      echo "we could not find any record";
+      exit;
+    }
+    mysqli_query($conn,"SET NAMES 'utf8'");
+
+    //get all recors from db
+    $sql= "SELECT * FROM competitors";
+    $result=mysqli_query($conn,$sql);
+    $records=[];
+
+    while($row=mysqli_fetch_assoc($result)){
+      $records[]=$row;
+    }
+
+    echo json_encode($records, JSON_UNESCAPED_UNICODE);
+    exit;
+
+    // $competidoresList=array(
+    //   array("name"=>"Ronald Andrés","lastName"=>"Púlido Gutierrez","score"=>0),
+    //   array("name"=>"Reymer","lastName"=>"Ramírez","score"=>0),
+    //   array("name"=>"Andrés Felipe","lastName"=>"Fandiño","score"=>0),
+    //   array("name"=>"Camilo","lastName"=>"Roa","score"=>0),
+    //   array("name"=>"José Alfredo","lastName"=>"Rueda Benítez","score"=>0),
+    //   array("name"=>"Camilo","lastName"=>"Orozco","score"=>0),
+    //   array("name"=>"Óscar","lastName"=>"Arana Correa","score"=>0),
+    //   array("name"=>"Juan Esteban","lastName"=>"Quíntero","score"=>0),
+    //   array("name"=>"Juan Felipe","lastName"=>"Ortega Mejía","score"=>0),
+    //   array("name"=>"Juan David","lastName"=>"Arroyave","score"=>0)
+    // );
+    // //print_r($competidoresList);
+    // echo json_encode($competidoresList);
   }
   //save in file competitors
   else if( $_SERVER['REQUEST_METHOD']=="POST" ){
